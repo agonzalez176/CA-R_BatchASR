f611e782063cf1885d2b09dec540bffe4f1264b9
Anthony <ag8896@nyu.edu>
2025-03-01 11:09:37 -0500

    revised batch_whisperx

--- batch_whisperx.py
+++ batch_whisperx.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 
-import sys, os, csv
-import time
+from datetime import datetime
+from enum import Enum
+
+import sys, os, csv, time, gc
 import argparse
-import gc
 
-import faster_whisper
-import whisperx, whisperx.utils
-import torch
+import faster_whisper, whisperx, whisperx.utils, torch
+from pymediainfo import MediaInfo
 
+class result_state(Enum):
+    ERROR = 0
+    SUCCESS = 1
 
+av_file_exts = ["wav","mp3","m4a","mov","mp4","webm","m4v","mpeg4"]
 
 # Read in input/output locations from command-line
 # Args:
@@ -33,36 +37,51 @@ def get_args():
         args = parser.parse_args()
         return args
 
-### INPUT VALIDATION ##########################################################
+def update_log(csv_writer, fpath, fname, msg, t_start, end_state):
+    now = datetime.now()
+    t_end = time.perf_counter()
+    elapsed_time = t_end - t_start
+
+    print(msg)
+    print("Elapsed time: ", elapsed_time, "\n")
+
+    try:
+        csv_writer.writerow([fpath, fname, elapsed_time,
+            msg, now.strftime("%Y/%m/%d %H:%M:%S", end_state)])
+        print("Wrote to results output file")
+    except:
+        print("Unable to write results to output file")
+
+def exit_msg(msg, msg_arg):
+    print(msg, msg_arg)
+    print("Exiting")
+    exit()
+
+
 def main():
+    ### INPUT VALIDATION ######################################################
     args = get_args()
 
     if not (os.path.exists(args.inlist)):
-        print("Filepath for inlist not found: ", args.inlist)
-        print("Exiting")
-        exit()
-
-    if not (os.path.exists(args.outdir)):
-        print("Output directory ", args.outdir, " not found. Creating now")
+        exit_msg("Filepath for inlist not found: ", args.inlist)
+    elif not (os.path.exists(args.outdir)):
+        print("Output directory ", args.outdir, 
+            " not found. Creating now")
         try:
             os.mkdir(args.outdir)
             print("Made output directory ", args.outdir)
         except:
-            print("Unable to create output directory at path: ", args.outdir)
-            print("Exiting")
-            exit()
-
-    if not (os.path.exists(args.outlist)):
-        print("Results output file not found at path: ", args.outlist)
-        print("Creating now")
-
+            exit_msg("Unable to create output directory at path: ", 
+                args.outdir)
+    elif not (os.path.exists(args.outlist)):
+        print("Output log ", args.outlist, " not found. Creating now")
         try:
             f = open(args.outlist, "w", newline='')
             f.close()
-            print("Created results output file at desired path: ", args.outlist)
+            print("Created output log at path: ", args.outlist)
         except:
-            print("Unable to create results output file at path: ", args.outlist)
-            exit()
+            exit_msg("Failed to create output log at path: ",
+             args.outlist)
     print("Validated args")
 
     ###############################################################################
@@ -75,8 +94,7 @@ def main():
     gc.collect(); torch.cuda.empty_cache()
     model = whisperx.load_model("distil-large-v3",
                                  device, 
-                                 compute_type=compute_type,
-                                 language="en")
+                                 compute_type=compute_type)
     print("Device: ", model.device)
     print("Set up model")
 
@@ -89,11 +107,12 @@ def main():
         # Check for properly formatted input CSV
         n_col = len(next(in_reader))
         if n_col != 3:
-            print("Unexpected number of columns in inlist file: ", args.inlist)
+            print("Unexpected number of columns in inlist file: ", 
+                args.inlist)
             print("Found ", n_col, " columns. Expected 3. Exiting")
-            inlist_obj.close(); outlist_obj.close()
+            inlist_obj.close()
             exit()
-        n_rows = sum(1 for row in inlist_obj)
+        n_rows = sum(1 for row in inlist_obj) + 1
         inlist_obj.seek(0)
 
 
@@ -101,6 +120,8 @@ def main():
             out_writer = csv.writer(outlist_obj, delimiter=',')
 
             i = 0
+            prev_result, prev_file = "", ""
+
             for row in in_reader:
                 t_start = time.perf_counter()
                 i+= 1
@@ -108,7 +129,8 @@ def main():
                 av_fpath, av_fname = row[0], row[1]
 
                 # Build filename for output transcript
-                out_fname = (os.path.splitext(av_fname))[0]
+                out_fname = (os.path.splitext(av_fname))[0] 
+                av_f_ext = ((os.path.splitext(av_fname))[1])[1:]
                 out_fname += ".vtt"
                 out_fpath = args.outdir + "/" + out_fname
 
@@ -117,23 +139,43 @@ def main():
 
                 # Check if target file exists
                 if not (os.path.exists(av_fpath)):
-                    print("Filepath does not exist for: ", av_fname)
-                    print("Filepath: ", av_fpath)
-                    print("Skipping transcription for: ", av_fname)
-
-                    t_end = time.perf_counter()
-                    print("Elapsed Time: ", t_end - t_start)
+                    print("Bad path: ", av_fpath)
+                    # Write to output log
+                    update_log(out_writer, fpath=av_fpath, fname=av_fname,
+                        msg="Target file does not exist. Skipping file.",
+                        t_start=t_start, end_state=result_state.ERROR.name)
+                    prev_file = av_fname
                     continue
-                
+
+                # Check if target file is an A/V file
+                if not(av_f_ext in av_file_exts):
+                    update_log(csv_writer=out_writer, fpath=av_fpath, 
+                        fname=av_fname,
+                        msg="Not a sound or video file. Skipping file.",
+                        t_start=t_start, end_state=result_state.ERROR.name)
+                    prev_file = av_fname
+                    continue
+
+                # Check if the file has already been transcribed
                 if (os.path.exists(out_fpath)):
-                    print("A transcript already exists for: ", av_fname)
-                    print("Transcript location: ", out_fpath, "\n")
-                    print("Skipping transcription")
-
-                    t_end = time.perf_counter()
-                    print("Elapsed Time: ", t_end - t_start)
+                    update_log(csv_writer=out_writer, fpath=av_fpath, 
+                        fname=av_fname,
+                        msg="This file has already been transcribed. Skipping file.",
+                        t_start=t_start, end_state=result_state.ERROR.name)
+                    prev_file = av_fname
                     continue
-     
+
+                # Check if the file has at least one audio track
+                file_mi = MediaInfo.parse(av_fpath)
+                if (len(file_mi.audio_tracks) == 0):
+                    update_log(csv_writer=out_writer, fpath=av_fpath, 
+                        fname=av_fname,
+                        msg="No audio tracks to transcribe. Skipping file.",
+                        t_start=t_start, end_state=result_state.ERROR.name)
+                    prev_file = av_fname
+                    continue
+
+                # Attempt transcription, write results to VTT file
                 try:
                     audio = whisperx.load_audio(av_fpath)
                     result = model.transcribe(
@@ -143,32 +185,40 @@ def main():
                         task="transcribe",
                         print_progress=True)
 
+                    # Check duplicate results here
+                    if result == prev_result:
+                        err_msg = "Duplicate VTT of " + prev_file
+                        update_log(csv_writer=out_writer, fpath=out_fpath,
+                            fname=av_fname, msg=err_msg, 
+                            t_start=t_start, 
+                            end_state=result_state.ERROR.name)
+                        prev_file = av_fname
+                        continue
+                    else:
+                        prev_result, prev_file = result, av_fname
+                    # Move txt writer here, for successful outputs
+                    try:
+                        txt_writer = whisperx.utils.get_writer("vtt", args.outdir)
+                        txt_writer(result, av_fname, 
+                            {
+                                "highlight_words": None,
+                                "max_line_count": None,
+                                "max_line_width": None   
+                            })
+                        print("Wrote to VTT: ", out_fname)
+                    except:
+                        print("Unable to write to VTT: ", out_fname)
                 except:
                     print("Unable to transcribe ", av_fname)
 
-                try:
-                    txt_writer = whisperx.utils.get_writer("vtt", args.outdir)
-                    txt_writer(result, av_fname, 
-                        {
-                            "highlight_words": None,
-                            "max_line_count": None,
-                            "max_line_width": None   
-                        })
-                    print("Wrote to VTT: ", out_fname)
-                except:
-                    print("Unable to write to VTT: ", out_fname)
-
-                try:
-                    out_writer.writerow([out_fpath, out_fname])
-                    print("Wrote to results output file")
-                except:
-                    print("Unable to write results to output file")
+                update_log(csv_writer=out_writer, fpath=out_fpath, 
+                    fname=out_fname,
+                    msg="Transcription successful",
+                    t_start=t_start, end_state=result_state.SUCCESS.name)
 
                 #delete model if low on GPU resources
                 gc.collect(); torch.cuda.empty_cache()
 
-                t_end = time.perf_counter()
-                print("Elapsed Time: ", t_end - t_start)
                 print("\n")
 
     print("Transcript file locations written to: " + args.outlist)
